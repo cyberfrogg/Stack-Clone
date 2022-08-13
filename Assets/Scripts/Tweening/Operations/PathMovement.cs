9795ffb77e882f460edf8142038e6defa59c91f1
cyberfrogg <68637510+cyberfrogg@users.noreply.github.com>
2022-08-14 01:34:59 +0300

    Tweener

---
+++ Assets/Scripts/Tweening/Operations/PathMovement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Tweening.Operations
+{
+    public class PathMovement : ITweenOperation
+    {
+        public event Action<ITweenOperation> Complete;
+        
+        private readonly Transform _transform;
+        private readonly Vector3[] _path;
+        private readonly float _speed;
+        private readonly float _accuracyTolerance;
+
+        private int _currentWaypointIndex;
+        private bool _isCompleted;
+        
+        public PathMovement(Transform transform, Vector3[] path, float speed, float accuracyTolerance = 0.1f)
+        {
+            _transform = transform;
+            _path = path;
+            _speed = speed;
+            _accuracyTolerance = accuracyTolerance;
+        }
+
+        public void Update()
+        {
+            if(_isCompleted)
+                return;
+            
+            var currentWaypoint = _path[_currentWaypointIndex];
+            _transform.position = Vector3.MoveTowards(_transform.position, currentWaypoint, _speed);
+
+            float distanceToCurrentWaypoint = Vector3.Distance(_transform.position, currentWaypoint);
+            if (distanceToCurrentWaypoint <= _accuracyTolerance)
+            {
+                SwitchToNextWaypoint();
+            }
+        }
+
+        private void SwitchToNextWaypoint()
+        {
+            _currentWaypointIndex++;
+
+            if (_currentWaypointIndex + 1 >= _path.Length)
+            {
+                CompleteOperation();
+            }
+        }
+
+        private void CompleteOperation()
+        {
+            _isCompleted = true;
+            Complete?.Invoke(this);
+        }
+    }
+}
--- Assets/Scripts/Tweening/SimpleTweener.cs
+++ Assets/Scripts/Tweening/SimpleTweener.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,17 @@ namespace Tweening
 {
     public class SimpleTweener : MonoBehaviour
     {
-        
+        private List<ITweenOperation> _tweenOperations = new();
+
+        public void Run(ITweenOperation operation)
+        {
+            operation.Complete += OnOperationComplete;
+            _tweenOperations.Add(operation);
+        }
+
+        private void OnOperationComplete(ITweenOperation operation)
+        {
+            operation.Complete -= OnOperationComplete;
+        }
     }
 }
