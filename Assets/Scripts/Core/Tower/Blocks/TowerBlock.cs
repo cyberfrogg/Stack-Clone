7ab2adcc264ebf598f336a556da7757da8a4a140
cyberfrogg <68637510+cyberfrogg@users.noreply.github.com>
2022-08-13 19:22:38 +0300

    Dumb dotween bug fixed

--- Assets/Scripts/Core/Tower/Blocks/TowerBlock.cs
+++ Assets/Scripts/Core/Tower/Blocks/TowerBlock.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
+using DG.Tweening.Core;
 using DG.Tweening.Plugins.Core.PathCore;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -13,8 +15,10 @@ namespace Core.Tower.Blocks
         [SerializeField, Min(0)] private float _blockSize = 5;
         [SerializeField, Min(0)] private float _movementDuration = 4;
         
-        private Sequence _movementSequence;
-        
+        private Tween _movementTween;
+        private Vector3 _currentTweenPosition;
+        private bool _isTweened;
+
         public void Destroy()
         {
             GameObject.Destroy(gameObject);
@@ -22,29 +26,37 @@ namespace Core.Tower.Blocks
 
         public void StartMovement(float yPosition, BlockMovementPathGenerator movementPathGenerator)
         {
+            _isTweened = true;
+            
             var waypoints = movementPathGenerator.GetNext(_blockSize, yPosition);
             AlignSelfAtStart(waypoints, yPosition);
             var path = new Path(PathType.Linear, waypoints.ToArray(), 5, Color.green);
-            
-            _movementSequence = DOTween.Sequence();
-            _movementSequence.Append(transform.DOPath(path,
+
+            transform.DOKill();
+            _movementTween = transform.DOPath(path,
                     _movementDuration,
                     PathMode.TopDown2D)
-                .SetEase(Ease.Linear)
-                .SetLoops(-1));
+                .SetEase(Ease.Linear).SetLoops(-1);
+        }
+        public void Drop()
+        {
+            transform.DOKill();
+            _movementTween?.Kill();
+            _movementTween = null;
+            transform.position = _isTweened ? _currentTweenPosition : transform.position;
+        }
+
+        private void Update()
+        {
+            _currentTweenPosition = transform.position;
         }
 
         private void AlignSelfAtStart(IEnumerable<Vector3> path, float yPosition)
         {
             transform.position = 
                 path.First().x == 0
-                ? new Vector3(transform.position.z, yPosition, _blockSize)
-                : new Vector3(_blockSize, yPosition, transform.position.z);
-        }
-
-        public void Drop()
-        {
-            _movementSequence?.Kill();
+                    ? new Vector3(transform.position.z, yPosition, _blockSize)
+                    : new Vector3(_blockSize, yPosition, transform.position.z);
         }
     }
 }
